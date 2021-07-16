8248c555b5c9b7e6851e24f3b76bab870dfb2161
Slaven <slaven.lakota@gmail.com>
2021-07-16 07:55:58 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a href="{{ url('/') }}">
-                    <img src="/ad_images/internet.jpg" width="170" height="70">
+                    <img src="/ad_images/oglasi3.png" width="190" height="70">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
